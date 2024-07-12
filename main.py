5b49facef1118bfc713169a1dbcda24ca5eb77ce
yshaposhnikov <pycakrus@ya.ru>
2024-07-12 12:03:13 +0200

    123

---
+++ main.py
@@ -0,0 +1,295 @@
+import cv2
+import numpy as np
+import pyautogui
+import time
+from PIL import ImageGrab
+from pynput import keyboard
+import pygame
+
+# Флаг для отслеживания состояния
+active = False
+
+
+
+pygame.mixer.init()
+
+
+def locate_image_on_screen(template_path):
+    # Снимок экрана
+    screenshot = ImageGrab.grab()
+    screenshot_np = np.array(screenshot)
+    screenshot_gray = cv2.cvtColor(screenshot_np, cv2.COLOR_BGR2GRAY)
+
+    # Шаблон изображения
+    template = cv2.imread(template_path, 0)
+    w, h = template.shape[::-1]
+
+    # Поиск шаблона на экране
+    res = cv2.matchTemplate(screenshot_gray, template, cv2.TM_CCOEFF_NORMED)
+    threshold = 0.88
+
+    loc = np.where(res >= threshold)
+    for pt in zip(*loc[::-1]):
+        return (pt[0] + w / 2, pt[1] + h / 2)
+    return None
+
+
+def click_on_image(image_path):
+    location = locate_image_on_screen(image_path)
+    if location:
+        pyautogui.click(location)
+
+
+def scenario1():
+    time.sleep(1)
+    click_on_image('visitsites.png')
+    visitsiteswait = locate_image_on_screen('visitsiteswait.png')
+    while visitsiteswait:
+        time.sleep(0.4)
+        visitsiteswait = locate_image_on_screen('visitsiteswait.png')
+    if locate_image_on_screen('ohnosites.png'):
+        click_on_image('back.png')
+        backwait = locate_image_on_screen('backwait.png')
+        while backwait:
+            time.sleep(0.5)
+            backwait = locate_image_on_screen('backwait.png')
+        print("переход к следующему сценарию.")
+        return
+    click_on_image('gotosite.png')
+    time.sleep(1)
+    click_on_image('open.png')
+    time.sleep(2)
+    if locate_image_on_screen('stay30sec.png'):
+        time.sleep(33)
+    if locate_image_on_screen('stay60sec.png'):
+        time.sleep(63)
+    if locate_image_on_screen('stay90sec.png'):
+        time.sleep(93)
+    if locate_image_on_screen('stay120sec.png'):
+        time.sleep(123)
+    # pyautogui.hotkey('alt', 'tab')
+    # pyautogui.click(270, 1060, button='left')
+    if locate_image_on_screen('visitsitestrap.png'):
+        click_on_image('visitsitestrap.png')
+        time.sleep(1)
+        if locate_image_on_screen('closeanyway.png'):
+            click_on_image('closeanyway.png')
+    if locate_image_on_screen('tgtray.png'):
+        click_on_image('tgtray.png')
+    #else:
+       # pyautogui.click(270, 1060, button='left')
+        #click_on_image('skip.png')
+    time.sleep(3)
+
+    if not(locate_image_on_screen('clickbeeisopen.png')):
+        click_on_image('clickbee.png')
+    if locate_image_on_screen('congratulations.png'):
+        pygame.mixer.music.load('beep.wav')
+        pygame.mixer.music.play()
+    #else:
+        #click_on_image('skip.png')
+
+
+def scenario2():
+    # Добавьте реализацию второго сценария здесь
+    time.sleep(1)
+    click_on_image('joinchannels.png')
+    time.sleep(1)
+    joinchannelswait = locate_image_on_screen('joinchannelswait.png')
+    while joinchannelswait:
+        time.sleep(0.2)
+        joinchannelswait = locate_image_on_screen('joinchannelswait.png')
+    if locate_image_on_screen('ohnochannels.png'):
+        print("переход к следующему сценарию.")
+        click_on_image('back.png')
+        backwait = locate_image_on_screen('backwait.png')
+        while backwait:
+            time.sleep(0.5)
+            backwait = locate_image_on_screen('backwait.png')
+        return
+    time.sleep(1)
+    if locate_image_on_screen('jointhechannel.png'):
+        click_on_image('jointhechannel.png')
+    time.sleep(1)
+    if locate_image_on_screen('joinchannel.png'):
+        click_on_image('joinchannel.png')
+    if locate_image_on_screen('alreadyjoined.png'):
+        click_on_image('clickbee.png')
+        time.sleep(1)
+        click_on_image('joined.png')
+        return
+    if locate_image_on_screen('joingroup.png'):
+        click_on_image('joingroup.png')
+    time.sleep(1)
+    if locate_image_on_screen('mute.png'):
+        click_on_image('mute.png')
+        time.sleep(1)
+    click_on_image('clickbee.png')
+    time.sleep(1)
+    click_on_image('joined.png')
+    time.sleep(1)
+    if locate_image_on_screen('congratulations.png'):
+        pygame.mixer.music.load('beep.wav')
+        pygame.mixer.music.play()
+
+
+
+
+def scenario3():
+    # Добавьте реализацию третьего сценария здесь
+
+
+    time.sleep(1)
+    click_on_image('joinbots.png')
+    time.sleep(1)
+    joinbotswait = locate_image_on_screen('joinbotswait.png')
+    while joinbotswait:
+        time.sleep(0.2)
+        joinbotswait = locate_image_on_screen('joinbotswait.png')
+    if locate_image_on_screen('ohnobots.png'):
+
+        click_on_image('back.png')
+        backwait = locate_image_on_screen('backwait.png')
+        while backwait:
+            time.sleep(0.5)
+            backwait = locate_image_on_screen('backwait.png')
+        return
+
+    time.sleep(2)
+    click_on_image('startthebot.png')
+    time.sleep(1)
+    if locate_image_on_screen('joinchannel.png'):
+        click_on_image('clickbee.png')
+        time.sleep(1)
+        click_on_image('skip.png')
+        return
+    if locate_image_on_screen('unmute.png'):
+        click_on_image('clickbee.png')
+        time.sleep(1)
+        click_on_image('skip.png')
+        time.sleep(1)
+        if locate_image_on_screen('skipohno.png'):
+            click_on_image('back.png')
+            backwait = locate_image_on_screen('backwait.png')
+            while backwait:
+                time.sleep(0.5)
+                backwait = locate_image_on_screen('backwait.png')
+        return
+    if locate_image_on_screen('ok.png'):
+        click_on_image('ok.png')
+        time.sleep(1)
+        click_on_image('3dots.png')
+        time.sleep(1)
+        click_on_image('openbot.png')
+        time.sleep(1)
+    if locate_image_on_screen('start.png'):
+        click_on_image('start.png')
+        time.sleep(5)
+    if locate_image_on_screen('youallowed.png'):
+        click_on_image('writeamessage.png')
+        pyautogui.typewrite('/start')
+        time.sleep(0.5)  # Небольшая пауза перед нажатием Enter
+        pyautogui.press('enter')
+        time.sleep(5)
+
+    location = locate_image_on_screen('botrespond.png')
+    location2 = locate_image_on_screen('botrespond2.png')
+    if location:
+        x, y = location
+        pyautogui.click(x + 50, y + 10, button='right')
+        time.sleep(1)
+        click_on_image('forward.png')
+        time.sleep(1)
+        click_on_image('clickbee2.png')
+        time.sleep(1)
+        click_on_image('started.png')
+        time.sleep(1)
+        pyautogui.press('enter')
+
+
+    if locate_image_on_screen('norespond.png') or locate_image_on_screen('norespond2.png'):
+        click_on_image('clickbee.png')
+        time.sleep(1)
+        click_on_image('skip.png')
+        time.sleep(1)
+        if locate_image_on_screen('skipohno.png'):
+            click_on_image('back.png')
+            backwait = locate_image_on_screen('backwait.png')
+            while backwait:
+                time.sleep(0.5)
+                backwait = locate_image_on_screen('backwait.png')
+        return
+    else:
+        #pyautogui.click(790, 900, button='right')
+        time.sleep(1)
+
+    #location = locate_image_on_screen('botrespond.png')
+
+    #time.sleep(1)
+    click_on_image('forward.png')
+    time.sleep(1)
+    click_on_image('clickbee2.png')
+    time.sleep(1)
+    click_on_image('started.png')
+    time.sleep(1)
+    pyautogui.press('enter')
+    time.sleep(1)
+    if locate_image_on_screen('skipohno.png'):
+        click_on_image('back.png')
+        backwait = locate_image_on_screen('backwait.png')
+        while backwait:
+            time.sleep(0.5)
+            backwait = locate_image_on_screen('backwait.png')
+
+        return
+    if locate_image_on_screen('congratulations.png'):
+        pygame.mixer.music.load('beep.wav')
+        pygame.mixer.music.play()
+        time.sleep(1)
+
+def scenario4():
+    # Добавьте реализацию четвертого сценария здесь
+    time.sleep(1)
+    click_on_image('more.png')
+    time.sleep(1)
+    click_on_image('viewposts.png')
+    time.sleep(1)
+    if locate_image_on_screen('ohnoposts.png'):
+        click_on_image('back.png')
+        return
+    else:
+        time.sleep(10)
+        click_on_image('watched.png')
+        time.sleep(1)
+        click_on_image('viewposts.png')
+        time.sleep(1)
+        click_on_image('back.png')
+        backwait = locate_image_on_screen('backwait.png')
+        while backwait:
+            time.sleep(0.5)
+            backwait = locate_image_on_screen('backwait.png')
+        time.sleep(1)
+def on_press(key):
+    global active
+    if key == keyboard.Key.caps_lock:
+        active = not active
+        print(f"{'Начало' if active else 'Остановка'} выполнения сценариев.")
+
+def main():
+    listener = keyboard.Listener(on_press=on_press)
+    listener.start()
+    print("Нажмите Caps Lock, чтобы начать/остановить выполнение сценариев.")
+
+    while True:
+        if active:
+            #scenario4()
+            scenario2()
+            scenario3()
+            scenario1()
+            time.sleep(1)
+        else:
+            time.sleep(1)
+
+
+if __name__ == "__main__":
+    main()
